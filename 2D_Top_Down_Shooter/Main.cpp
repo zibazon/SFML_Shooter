37873294219fb1dd5344ab5ddb87d4bb0e3096df
Sean Smith <hakk3d@aim.com>
2013-04-29 15:44:34 -0400

    Got a window up!
    Created a namespace to manage "Globals" and such, got a window running
    and other stuff.

--- 2D_Top_Down_Shooter/Main.cpp
+++ 2D_Top_Down_Shooter/Main.cpp
@@ -2,12 +2,27 @@
 #include "Main.h"
 
 
-
-
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
 
-	//Create Class to handle main and just instantiate from there
+
+	Game::Init();
+
+
+	while (Game::MainWindow->isOpen())
+    {
+        sf::Event event;
+        while (Game::MainWindow->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                Game::MainWindow->close();
+        }
+
+        Game::MainWindow->clear();
+
+        Game::MainWindow->display();
+    }
+
 
 
     return 0;
--- 2D_Top_Down_Shooter/Main.h
+++ 2D_Top_Down_Shooter/Main.h
@@ -6,27 +6,45 @@
 
 
 
-//This is the main Class, it handles all "Globals"
+
+void RenderWindow(sf::RenderWindow * RenderWindow)
+{
+
+
+
+}
+
+
+
+
+//This is the main namespace, it handles all "Globals"
 //GameState, window messages rendering and manages all subsequent classes
 
 
 
-class Game {
+namespace Game {
 
-public:
 
-	//Construct, Create the window, start rendering, initiate the game/menus etc.
-	Game();
+	//Global Variables
 
-	//Deconstruct, Game Exit
-	~Game();
-
-	
-private:
 	int GameState; //What menu are we in? What should be rendered/not rendered
-
 	sf::RenderWindow * MainWindow; //Our main render window
+
+
+
+	//Initialize startup variables, Create the window, start rendering, initiate the game/menus etc.
+	void Init()
+	{
+
+		//Initialize our render window
+		MainWindow = new sf::RenderWindow(sf::VideoMode(1280, 720), "Game");
+
+	}
+
+
 	
+
+	//Store information on screen resolution and other simple settings like that
 
 };
 
